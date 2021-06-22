d2762a736b6fbb3bcd928cb8311dd790e981c5d9
jainilsoni1706 <75731365+jainilsoni1706@users.noreply.github.com>
2021-06-23 02:26:10 +0530

    Add files via upload

---
+++ indexETC.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <script src="main.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- LoadingSection -->
+
+
+
+<div class="LoadingUpperSvg">
+    <!-- <img src="wave.svg"> -->
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#ffd700" fill-opacity="1" d="M0,64L34.3,80C68.6,96,137,128,206,154.7C274.3,181,343,203,411,176C480,149,549,75,617,74.7C685.7,75,754,149,823,154.7C891.4,160,960,96,1029,74.7C1097.1,53,1166,75,1234,85.3C1302.9,96,1371,96,1406,96L1440,96L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+</svg>
+</div>
+
+<div class="Loading">
+<img src="w.png" class="Loading_Letter">
+<img src="e.png" class="Loading_Letter">
+<img src="l.png" class="Loading_Letter">
+<img src="c.png" class="Loading_Letter">
+<img src="o.png" class="Loading_Letter">
+<img src="m.png" class="Loading_Letter">
+<img src="e.png" class="Loading_Letter">
+</div>
+
+
+<div class="LoadingDownSvg">
+    <img src="wave.svg">
+</div>
+
+
+<!-- LoadingSection -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- comments -->
+
+<!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#000b76" fill-opacity="1" d="M0,64L34.3,80C68.6,96,137,128,206,154.7C274.3,181,343,203,411,176C480,149,549,75,617,74.7C685.7,75,754,149,823,154.7C891.4,160,960,96,1029,74.7C1097.1,53,1166,75,1234,85.3C1302.9,96,1371,96,1406,96L1440,96L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+</svg> -->
---
+++ main.js
@@ -0,0 +1,205 @@
+
+setTimeout(function(){
+    document.querySelector('.Loading_Letters').style.display = "none";
+    document.querySelector('.row_LoadingLine').style.display = "none";
+    document.querySelector('body').style.background = "#f3f4f5";
+    document.querySelector('main').style.display = "block";
+},10000);
+
+setTimeout(function(){
+    document.querySelector('.row_LoadingLine').style.display = "none";
+    },15000);
+
+
+setTimeout(function(){
+let x = document.querySelector('.logoMain');
+x.src = "j.png";
+x.style.height = "10vh";
+x.style.width = "10vh";
+x.style.border = "3px solid white";
+x.style.padding = "3px";
+x.style.borderRadius = "100%";
+},7000);
+
+
+
+
+let x1 = document.querySelector('.NaVMeNu').style.display = "block"; 
+let y1 = document.querySelector('.MainBtnBoX').style.display = "block";
+
+if(x1&&y1){
+    document.querySelector('.MainBtnBoX').style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let NaMe = document.querySelector('.InpuTnAmE').value;
+//   if(NaMe.length>8){
+// alert("OOPS.........")
+//   }
+
+//   else if(NaMe.length==""){
+//     alert("OOPS.........")
+//   }
+
+
+// if(NaMe=="a" || NaMe=="A"){
+//   console.log(a);
+//   let nx = document.createElement('input');
+//   nx.type = "image";
+//   nx.src = a;
+//   nx.style.height = "50px";
+//   nx.style.width = "50px";
+//   document.body.appendChild(nx);
+// }
+
+// else if(NaMe=="b" || NaMe=="B"){
+//   console.log(b);
+// }
+
+// else if(NaMe=="c" || NaMe=="C"){
+//   console.log(c);
+// }
+// else if(NaMe=="d" || NaMe=="D"){
+//   console.log(d);
+// }
+// else if(NaMe=="e" || NaMe=="E"){
+//   console.log(e);
+// }
+// else if(NaMe=="f" || NaMe=="F"){
+//   console.log(f);
+// }
+// else if(NaMe=="g" || NaMe=="G"){
+//   console.log(g);
+// }
+// else if(NaMe=="h" || NaMe=="H"){
+//   console.log(h);
+// }
+// else if(NaMe=="i" || NaMe=="I"){
+//   console.log(i);
+// }
+// else if(NaMe=="j" || NaMe=="J"){
+//   console.log(j);
+//   let nx1 = document.createElement('input');
+//   nx1.type = "image";
+//   nx1.src = j;
+//   nx1.style.height = "50px";
+//   nx1.style.width = "50px";
+//   document.body.appendChild(nx1);
+// }
+// else if(NaMe=="k" || NaMe=="K"){
+//   console.log(k);
+// }
+// else if(NaMe=="l" || NaMe=="L"){
+//   console.log(l);
+// }
+// else if(NaMe=="m" || NaMe=="M"){
+//   console.log(m);
+// }
+// else if(NaMe=="n" || NaMe=="N"){
+//   console.log(n);
+// }
+// else if(NaMe=="o" || NaMe=="O"){
+//   console.log(o);
+// }
+// else if(NaMe=="p" || NaMe=="P"){
+//   console.log(p);
+// }
+// else if(NaMe=="q" || NaMe=="Q"){
+//   console.log(q);
+// }
+// else if(NaMe=="r" || NaMe=="R"){
+//   console.log(r);
+// }
+// else if(NaMe=="s" || NaMe=="S"){
+//   console.log(s);
+// }
+// else if(NaMe=="t" || NaMe=="T"){
+//   console.log(t);
+// }
+// else if(NaMe=="u" || NaMe=="U"){
+//   console.log(u);
+// }
+// else if(NaMe=="v" || NaMe=="V"){
+//   console.log(v);
+// }
+// else if(NaMe=="w" || NaMe=="W"){
+//   console.log(w);
+// }
+// else if(NaMe=="x" || NaMe=="X"){
+//   console.log(x);
+// }
+// else if(NaMe=="y" || NaMe=="Y"){
+//   console.log(y);
+// }
+// else if(NaMe=="z" || NaMe=="Z"){
+//   console.log(z);
+// }
+
+
+
+// a = "a.png";
+// b = "b.png";
+// c = "c.png"; 
+// d = "d.png";
+// e = "e.png";
+// f = "f.png"
+// g = "g.png";
+// h = "h.png";
+// i = "i.png";
+// j = "j.png";
+// k = "k.png";
+// l = "l.png";
+// m = "m.png";
+// n = "n.png";
+// o = "o.png";
+// p = "p.png";
+// q = "q.png";
+// r = "r.png";
+// s = "s.png";
+// t = "t.png";
+// u = "u.png";
+// v = "v.png";
+// w = "w.png";
+// x = "x.png";
+// y = "y.png";
+// z = "z.png";
